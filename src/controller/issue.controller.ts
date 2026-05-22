8600dd8025c89feda7c08ad35a2ae84052da825e
ars2k03 <arafatsohan2003@gamil.com>
2026-05-22 19:26:35 +0600

    Implement CRUD operations for issues and update routing structure

--- src/controller/issue.controller.ts
+++ src/controller/issue.controller.ts
@@ -41,10 +41,7 @@ export const createIssue = async ( req: Request, res: Response ) => {
   }
 };
 
-export const getAllIssues = async (
-  req: Request,
-  res: Response
-) => {
+export const getAllIssues = async (req: Request, res: Response) => {
   try {
     const queryParams = req.query;
 
@@ -128,6 +125,198 @@ export const getAllIssues = async (
     res.status(500).json({
       success: false,
       message: "Failed to fetch issues",
+    });
+
+  }
+};
+
+export const getSingleIssue = async (req: Request,res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const issueQuery = `
+    SELECT * FROM issues
+    WHERE id = $1
+    `;
+
+    const issueResult = await pool.query(
+      issueQuery,
+      [id]
+    );
+
+    const issue = issueResult.rows[0];
+
+
+    if (!issue) {
+      throw new Error("Issue not found");
+    }
+
+
+    const userQuery = `
+      SELECT id, name, role
+      FROM users
+      WHERE id = $1
+    `;
+
+    const userResult = await pool.query(
+      userQuery,
+      [issue.reporter_id]
+    );
+
+    const reporter = userResult.rows[0];
+
+    const result = {
+      id: issue.id,
+      title: issue.title,
+      description: issue.description,
+      type: issue.type,
+      status: issue.status,
+      reporter,
+      created_at: issue.created_at,
+      updated_at: issue.updated_at,
+    }
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+
+export const updateIssue = async ( req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user = req.user;
+
+    const data = req.body;
+
+    const findQuery = `
+    SELECT * FROM issues
+    WHERE id = $1
+    `;
+
+    const issueResult = await pool.query(
+      findQuery,
+      [id]
+    );
+
+    const issue = issueResult.rows[0];
+
+    if (!issue) {
+      throw new Error("Issue not found");
+    }
+
+    if (user.role !== "maintainer") {
+      
+      if (issue.reporter_id !== user.id) {
+        throw new Error(
+          "You are not allowed to update this issue"
+        );
+      }
+
+      if (issue.status !== "open") {
+        throw new Error(
+          "Only open issues can be updated"
+        );
+      }
+    }
+
+    const updateQuery = `
+      UPDATE issues
+      SET
+        title = $1,
+        description = $2,
+        type = $3,
+        updated_at = CURRENT_TIMESTAMP
+      WHERE id = $4
+      RETURNING *
+    `;
+
+    const values = [
+      data.title,
+      data.description,
+      data.type,
+      id,
+    ];
+
+    const result = await pool.query(
+      updateQuery,
+      values
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Issue updated successfully",
+      data: result.rows[0],
+    });
+
+  } catch (error) {
+
+    res.status(403).json({
+      success: false,
+      message: (error as Error).message,
+    });
+
+  }
+};
+
+export const deleteIssue = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const user = req.user;
+
+    if (user.role !== "maintainer") {
+      throw new Error(
+        "Only maintainer can delete issues"
+      );
+    }
+    
+    const findQuery = `
+      SELECT * FROM issues
+      WHERE id = $1
+    `;
+
+    const issueResult = await pool.query(
+      findQuery,
+      [id]
+    );
+
+    const issue = issueResult.rows[0];
+
+    if (!issue) {
+      throw new Error("Issue not found");
+    }
+
+
+    const deleteQuery = `
+      DELETE FROM issues
+      WHERE id = $1
+    `;
+
+    await pool.query(deleteQuery, [id]);
+
+    res.status(200).json({
+      success: true,
+      message: "Issue deleted successfully",
+    });
+
+  } catch (error) {
+
+    res.status(403).json({
+      success: false,
+      message: (error as Error).message,
     });
 
   }
--- src/router/server.route.ts
+++ src/router/server.route.ts
@@ -2,13 +2,19 @@ import express from "express";
 import { signup } from "../controller/auth.signup";
 import { login } from "../controller/auth.login";
 import { auth } from "../middleware/auth.middle";
-import { createIssue, getAllIssues } from "../controller/issue.controller";
+import { createIssue, deleteIssue, getAllIssues, getSingleIssue, updateIssue } from "../controller/issue.controller";
 
 const router = express.Router();
 
 router.post('/auth/signup', signup);
 router.post('/auth/login', login);
-router.post('/issues', auth, createIssue);
-router.get('/issues', getAllIssues);
+router.route('/issues')
+.post( auth, createIssue)
+.get(getAllIssues);
+
+router.route('/issues/:id')
+.get(getSingleIssue)
+.patch( auth, updateIssue)
+.delete(auth, deleteIssue);
 
 export default router;
